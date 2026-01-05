833aa8e178aab9f154660fc556250c0c4066f8a9
valentin-vinel <valentinvnl84@gmail.com>
2026-01-05 10:37:41 +0100

    refactor: clean up users controller

--- src/app/controllers/app-user.controller.ts
+++ src/app/controllers/app-user.controller.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { AppUser } from "../models/app-user.model.js";
 
 // Endpoint: Get all app-users
-export const getAll = async(req: Request, res: Response) => {
+export const listUsers = async(req: Request, res: Response) => {
     try {
         const users = await AppUser.findAll()
 
-        res.json(users);
+        res.status(200).json(users);
     } catch (error) {
-        console.log("Error fetching users data : ", error)
+        console.error("Error fetching users data : ", error)
         res.status(500).json({ error: "Failed to fetch users data."})
     }
 }
--- src/app/routers/router.ts
+++ src/app/routers/router.ts
@@ -3,4 +3,4 @@ import * as appUserController from "../controllers/app-user.controller.js"
 
 export const router = Router();
 
-router.get('/users', appUserController.getAll);
+router.get('/users', appUserController.listUsers);
