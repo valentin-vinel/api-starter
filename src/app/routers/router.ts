b7cf06af960fb3404e6ff98c3dfb8deb5a4874f7
valentin-vinel <valentinvnl84@gmail.com>
2026-01-06 08:59:18 +0100

    feat(router): add /users/:id

--- src/app/routers/router.ts
+++ src/app/routers/router.ts
@@ -4,3 +4,4 @@ import * as appUserController from "../controllers/app-user.controller.js"
 export const router = Router();
 
 router.get('/users', appUserController.listUsers);
+router.get('/users/:id', appUserController.getOneUser);
