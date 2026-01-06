1041e9a5711da818524bd2f5d79d41093e3a671b
valentin-vinel <valentinvnl84@gmail.com>
2026-01-06 10:52:49 +0100

    feat(controller): add get one ressource endpoint

--- src/app/controllers/ressource.controller.ts
+++ src/app/controllers/ressource.controller.ts
@@ -13,3 +13,21 @@ export const listRessources = async(req: Request, res: Response) => {
         res.status(500).json({ error: "Failed to fetch ressources data."})
     }
 }
+
+// Endpoint: Get one ressource
+export const getOneRessource = async(req: Request, res: Response) => {
+    try {
+        const { id } = idSchema.parse(req.params);
+
+        const ressource = await Ressource.findByPk(id);
+
+        if (!ressource) {
+            return res.status(404).json({ message: "Ressource not found" });
+        };
+
+        res.json(ressource)
+    } catch (error) {
+        console.error("Error fetching ressource data : ", error)
+        res.status(500).json({ error: "Failed to fetch ressource data."})
+    }
+}
